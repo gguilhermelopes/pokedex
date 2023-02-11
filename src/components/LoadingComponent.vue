8727072a59687411ce8291b4c14c1ad1da9fd78e
gguilhermelopes <guilhermelopes275@gmail.com>
2023-02-11 00:48:01 -0300

    feat: loading page built

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,3 +1,28 @@
+<script setup>
+import SvgIcon from "./icons/SvgIcon.vue";
+</script>
+
 <template>
-  <p>Carregando...</p>
+  <div class="loading">
+    <SvgIcon />
+  </div>
 </template>
+
+<style lang="scss">
+.loading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    animation: spin 0.5s infinite;
+    width: 70%;
+    opacity: 0.8;
+  }
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(720deg);
+  }
+}
+</style>
